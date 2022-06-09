4ccfdf30c495ce470eada4b02fe111a49d039918
Adam Kadlec <adam.kadlec@gmail.com>
2022-06-09 22:47:33 +0200

    Upgrading packages

--- fastybird_sonoff_connector/__init__.py
+++ fastybird_sonoff_connector/__init__.py
@@ -20,7 +20,7 @@ Sonoff connector module
 
 from .bootstrap import create_connector
 
-__version__ = "0.2.0"
+__version__ = "0.3.0"
 
 __all__ = ["connector", "bootstrap", "__connector_version__"]
 
